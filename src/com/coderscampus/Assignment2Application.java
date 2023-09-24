65a1a7f4de5bd52d82852cb006a1d8e67fd235b3
LordEricDraven <c.flamion88@gmail.com>
2023-09-24 16:48:34 -0400

    V2 changes

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -5,6 +5,14 @@ public class Assignment2Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// generate a random number
+		
+		// and collect user input
+		
+		// etc.
+		
+		
+		
 	}
 
 }
